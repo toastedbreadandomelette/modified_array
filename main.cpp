445cb2dd18094cbf490bb4f7596b96eb3382c72b
toastedbreadandomelette <toastedbreadandomelette@gmail.com>
2023-06-15 13:12:50 +0530

    Temp commit 2

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 
 int main(i32 argc, const char **argv) {
     // MdStaticArray<double>::set_threshold_size(1000);
-    usize sz = 4096;
+    usize sz = 8192;
     // MdStaticArray<double>::set_thread_count(1);
     Array<f64> c({sz, sz}, 0);
     for (usize i = 0; i < sz; ++i) {
@@ -23,7 +23,7 @@ int main(i32 argc, const char **argv) {
     std::cout << c.get_size() << '\n';
 
     auto start = std::chrono::system_clock::now();
-    auto ans = FFT::ifft2<f64>(FFT::fft2<f64>(c));
+    auto ans = FFT::fft2<f64>(c);
     auto end = std::chrono::system_clock::now();
 
     std::chrono::duration<f64> time = end - start;
