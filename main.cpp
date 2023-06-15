a674e7f383c5b2373c554bb5b592835e1e1f6b06
toastedbreadandomelette <toastedbreadandomelette@gmail.com>
2023-06-15 13:11:50 +0530

    Temp commit

--- main.cpp
+++ main.cpp
@@ -13,18 +13,17 @@
 
 int main(i32 argc, const char **argv) {
     // MdStaticArray<double>::set_threshold_size(1000);
-    usize sz = 2048;
+    usize sz = 4096;
     // MdStaticArray<double>::set_thread_count(1);
-    Array<f32> c({sz, sz}, 0), d({sz, sz}, 0);
+    Array<f64> c({sz, sz}, 0);
     for (usize i = 0; i < sz; ++i) {
-        c[i] = Utils::range<f32>(i * sz, i * sz + sz);
-        d[i] = Utils::range<f32>(i * sz, i * sz + sz);
+        c[i] = Utils::range<f64>(i * sz, i * sz + sz);
     }
 
     std::cout << c.get_size() << '\n';
 
     auto start = std::chrono::system_clock::now();
-    auto ans = Linalg::mat_multiply<f32>(c, d, 16);
+    auto ans = FFT::ifft2<f64>(FFT::fft2<f64>(c));
     auto end = std::chrono::system_clock::now();
 
     std::chrono::duration<f64> time = end - start;
